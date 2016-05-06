d8679e608eccb0c5606f41004287b6c0bc93197c
Chris-Kay <chris.kay01@bbc.co.uk>
2016-05-06 09:06:29 +0100

    Done spare

--- spec/score-card-spec.js
+++ spec/score-card-spec.js
@@ -1,8 +1,28 @@
 var scoreCard = require('../src/score-card.js');
 
 describe('The bowling scorecard', function() {
+
+	beforeEach(function() {
+		scoreCard.reset();
+	});
+
 	it('registers a score', function() {
 		scoreCard.bowl(3);
 		expect(scoreCard.getScore()).toBe(3);
+	});
+
+	it('registers multiple scores', function() {
+		scoreCard.bowl(3);
+		scoreCard.bowl(6);
+
+		expect(scoreCard.getScore()).toBe(9);	
+	});
+
+	it('registers a spare', function() {
+		scoreCard.bowl(3);
+		scoreCard.bowl(7); 
+		scoreCard.bowl(2); //Doubled 
+
+		expect(scoreCard.getScore()).toBe(14);	
 	});
 });
--- src/score-card.js
+++ src/score-card.js
@@ -1,14 +1,40 @@
-var score = 0;
+var round = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+current = 0;
 
-function bowl(roundScore) {
-	score += roundScore;
+function bowl(roll) {
+	round[current++] = roll;
+}
+
+function isSpare(round, i) {
+	return round[i] + round[i + 1] === 10
 }
 
 function getScore() {
+	var score = 0,
+	i = 0;
+
+	for(var frame = 0; frame < 10; frame+=1) {
+		if(isSpare(round, i)) {
+			score += 10 + round[i + 2];
+			i += 2;
+		} else {
+			// Open Frame
+			score += round[i] + round[i + 1];
+			i += 2;
+		}
+	}
+
 	return score;
+}
+
+function reset() {
+	score = 0;
+	round = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	current = 0;
 }
 
 module.exports = {
 	bowl: bowl,
-	getScore: getScore
+	getScore: getScore,
+	reset: reset
 }
